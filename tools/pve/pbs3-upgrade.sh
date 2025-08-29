9982439590d5d3f30cd530aa122c6e6265ba000b
serogaq <36307024+serogaq@users.noreply.github.com>
2025-08-29 22:13:30 +0300

    Update pbs3-upgrade.sh

--- tools/pve/pbs3-upgrade.sh
+++ tools/pve/pbs3-upgrade.sh
@@ -67,9 +67,9 @@ start_routines() {
   yes)
     msg_info "Changing to Proxmox Backup Server 3 Sources"
     cat <<EOF >/etc/apt/sources.list
-deb http://deb.debian.org/debian bookworm main contrib
-deb http://deb.debian.org/debian bookworm-updates main contrib
-deb http://security.debian.org/debian-security bookworm-security main contrib
+deb https://deb.debian.org/debian bookworm main contrib
+deb https://deb.debian.org/debian bookworm-updates main contrib
+deb https://security.debian.org/debian-security bookworm-security main contrib
 EOF
     msg_ok "Changed to Proxmox Backup Server 3 Sources"
     ;;
